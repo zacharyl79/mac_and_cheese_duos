70a0f47fed02a77be87bea7f73ee3e609cff5244
zacharyl79 <zacharyl79@nycstudents.net>
2025-03-28 09:32:34 -0400

    will fix over the weekend

--- little-lemon/src/Components/CallToAction.js
+++ little-lemon/src/Components/CallToAction.js
@@ -1,4 +1,5 @@
 import FoodImage from "../assets/restauranfood.png";
+import { Link } from "react-router-dom"
 
 const CallToAction = () => {
     return (
@@ -7,7 +8,9 @@ const CallToAction = () => {
                 <h1>Little Lemon</h1>
                 <h3>Chicago</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <button>Reserve A Table</button>
+                <Link to="/booking" style={{"text-decoration": "none"}}>
+                    <button className="reservation-button">Reserve A Table</button>
+                </Link>
             </article>
             <img src={FoodImage} alt="Coursera Provided"/>
         </section>
--- little-lemon/src/Pages/BookingPage.js
+++ little-lemon/src/Pages/BookingPage.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const BookingPage = () => {
+    const [formInfo, setFormInfo] = useState({
+        "date": Date(),
+        "time": "8:30 PM",
+        "guests": 1,
+        "occasion": "None"
+    });
+
+    const handleChanges = (e, inputKind) => {
+        e.preventDefault();
+        setFormInfo(prevState => {...prevState, inputKind: e.value});
+    };
+
     return (
-        <>
-        </>
+        <form style={{"display": "grid", "max-width" : "200px", "gap": "20px"}}>
+            <label for="res-date">Choose date: </label>
+            <input type="date" id="res-date" value={formInfo.date} onChange={}/>
+            <label for="res-time">Choose time: </label>
+            <select id="res-time" value={formInfo.time}>
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </select>
+            <label for="guests">Number of guests: </label>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" value={formInfo.guests}/>
+            <label for="occasion">Occasion: </label>
+            <select id="occasion" value={formInfo.occasion}>
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </select>
+            <input type="submit" value="Make Your reservation" />
+        </form>
     );
 };
 
