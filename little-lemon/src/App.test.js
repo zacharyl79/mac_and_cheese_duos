320bdde81fdc20928c6df2fbb0439b101d0f396e
Zachary Lin <144487346+zacharyl79@users.noreply.github.com>
2025-04-04 23:25:12 -0400

    Delete little-lemon directory

--- little-lemon/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import BookingForm from './BookingForm';
-
-test('Renders the BookingForm heading', () => {
-    render(<BookingForm />);
-    const headingElement = screen.getByText("Make Your Reservation");
-    expect(headingElement).toBeInTheDocument();
-})
