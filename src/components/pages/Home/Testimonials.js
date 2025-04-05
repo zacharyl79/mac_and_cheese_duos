9cd23978d98026a5fe77c07f882d3d7b591cdbde
Zachary Lin <144487346+zacharyl79@users.noreply.github.com>
2025-04-04 23:30:35 -0400

    committed local project

---
+++ src/components/pages/Home/Testimonials.js
@@ -0,0 +1,52 @@
+import customer1Image from './assets/customer1.jpg';
+import customer2Image from './assets/customer2.jpg';
+import customer3Image from './assets/customer3.jpg';
+import customer4Image from './assets/customer4.jpg';
+import './Testimonials.css';
+import TestimonialCard from './TestimonialCard';
+
+const customers = [
+  {
+    fullName: 'Person 1',
+    image: customer1Image,
+    rating: [1, 1, 1, 1, 0.5],
+    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    fullName: 'Person 2',
+    image: customer2Image,
+    rating: [1, 1, 1, 1, 1],
+    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    fullName: 'Person 3',
+    image: customer3Image,
+    rating: [1, 1, 1, 1, 0.5],
+    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+  {
+    fullName: 'Person 4',
+    image: customer4Image,
+    rating: [1, 1, 1, 1],
+    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  },
+];
+
+const Testimonials = () => {
+  return (
+    <section className="testimonials">
+      <div className="container grid">
+        <h2>What people say about us!</h2>
+        {customers.map((customer, index) => 
+          <TestimonialCard key={index} customer={customer} />
+        )}
+     </div>
+    </section>
+  );
+};
+
+export default Testimonials;
